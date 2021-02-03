39f79bd77a9c10b991523110852c78491d06fa15
Jeannie Finks (NM) <74554921+jeanniefinks@users.noreply.github.com>
2021-02-03 14:03:43 -0500

    GA number added, trailing URL slash fix

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -86,6 +86,12 @@ pygments_style = "sphinx"
 html_theme = "sphinx_rtd_theme"
 html_logo = "icon-engine.png"
 
+html_theme_options = {
+    'analytics_id': 'UA-128364174-1',  #  Provided by Google in your dashboard
+    'analytics_anonymize_ip': False,
+}
+
+
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
