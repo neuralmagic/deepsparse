cf4e6bf0b810f696d7bb292638cf8b685e71537e
Mark Kurtz <mark@neuralmagic.com>
2021-04-07 10:25:55 -0400

    Set branch whitelist to main now that docs-upgrades changes have landed (#77)

--- docs/conf.py
+++ docs/conf.py
@@ -64,7 +64,7 @@ templates_path = ["_templates"]
 smv_tag_whitelist = r'^v.*$'
 
 # Whitelist pattern for branches (set to None to ignore all branches)
-smv_branch_whitelist = r'^(main)|(docs-upgrades)$'
+smv_branch_whitelist = r'^main$'
 
 # Whitelist pattern for remotes (set to None to use local branches only)
 smv_remote_whitelist = r'^.*$'
