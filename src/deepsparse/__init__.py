f8c9c5678b2624914094adde531bf53fb67e45c2
George Ohashi <george@neuralmagic.com>
2021-11-15 15:28:43 -0500

    added try catch for sparsezoo package

--- src/deepsparse/__init__.py
+++ src/deepsparse/__init__.py
@@ -30,9 +30,17 @@ from .engine import *
 from .version import *
 
 
-from sparsezoo.package import check_package_version as _check_package_version
+try:
+    from sparsezoo.package import check_package_version as _check_package_version
 
-_check_package_version(
-    package_name=__name__ if is_release else f"{__name__}-nightly",
-    package_version=version,
-)
+    is_release = True
+    _check_package_version(
+        package_name=__name__ if is_release else f"{__name__}-nightly",
+        package_version=version_base,
+    )
+except Exception as err:
+    print(
+        f"Need sparsezoo version above 0.9.0 to run Neural Magic's latest-version check\n{err}"
+    )
+
+
