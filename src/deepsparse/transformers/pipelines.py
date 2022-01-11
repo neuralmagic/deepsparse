4b18ae3a293153cf3f5f0824ccadddd8d4cc1a82
Rahul Tuli <rahul@neuralmagic.com>
2022-01-11 10:48:38 -0500

    Fix: Style

--- src/deepsparse/transformers/pipelines.py
+++ src/deepsparse/transformers/pipelines.py
@@ -45,7 +45,6 @@ from deepsparse.transformers.helpers import (
     get_onnx_path_and_configs,
     overwrite_transformer_onnx_model_inputs,
 )
-
 from deepsparse.transformers.loaders import get_batch_loader
 
 
