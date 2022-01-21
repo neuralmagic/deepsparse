8a27ae05ce7a452ca66cddaa13439742935e1605
Benjamin Fineran <bfineran@users.noreply.github.com>
2022-01-21 09:44:42 -0500

    update transformers deps to install on import (#246)

--- src/deepsparse/transformers/__init__.py
+++ src/deepsparse/transformers/__init__.py
@@ -19,22 +19,87 @@ huggingface/transformers
 
 # flake8: noqa
 
+import logging as _logging
+
+
 try:
     import transformers as _transformers
 
-    transformers_import_error = None
-except Exception as transformers_import_err:
-    transformers_import_error = transformers_import_err
+    _transformers_import_error = None
+except Exception as _transformers_import_err:
+    _transformers_import_error = _transformers_import_err
+
+
+_LOGGER = _logging.getLogger(__name__)
+
+
+def _install_transformers_and_deps():
+
+    import deepsparse as _deepsparse
+    import pip as _pip
+
+    transformers_branch = (
+        "master"
+        if not _deepsparse.is_release
+        else f"release/{_deepsparse.version.version_major_minor}"
+    )
+    transformers_requirement = (
+        "transformers @ git+https://github.com/neuralmagic/transformers.git"
+        f"@{transformers_branch}"
+    )
+
+    try:
+        _pip.main(
+            [
+                "install",
+                transformers_requirement,
+                "datasets",
+                "sklearn",
+                "seqeval",
+            ]
+        )
+
+        import transformers as _transformers
+
+        _LOGGER.info("deepsparse-transformers and dependencies successfully installed")
+    except Exception:
+        raise ValueError(
+            "Unable to install and import deepsparse-transformers dependencies check "
+            "that transformers is installed, if not, install via "
+            "`pip install git+https://github.com/neuralmagic/transformers.git`"
+        )
 
 
 def _check_transformers_install():
-    if transformers_import_error is None:
-        return
-    raise ImportError(
-        "No installation of transformers found. It is recommended to use the "
-        "sparseml fork of transformers which can be installed under "
-        "git+https://github.com/neuralmagic/transformers.git or sparseml[transformers]"
-    )
+    if _transformers_import_error is not None:
+        import os
+
+        if os.getenv("NM_NO_AUTOINSTALL_TRANSFORMERS", False):
+            _LOGGER.warning(
+                "Unable to import transformers, skipping auto installation "
+                "due to NM_NO_AUTOINSTALL_TRANSFORMERS"
+            )
+            # skip any further checks
+            return
+        else:
+            _LOGGER.info(
+                "No installation of transformers found. Installing deepsparse-transformers "
+                "dependencies"
+            )
+            _install_transformers_and_deps()
+
+    # check NM fork installed with QATMatMul available
+    try:
+        import transformers as _transformers
+
+        _transformers.models.bert.modeling_bert.QATMatMul
+    except Exception:
+        _LOGGER.warning(
+            "transformers.models.bert.modeling_bert.QATMatMul not availalbe. the"
+            "neuralmagic fork of transformers may not be installed. it can be "
+            "installed via "
+            "`pip install git+https://github.com/neuralmagic/transformers.git`"
+        )
 
 
 _check_transformers_install()
