8f83fadaa7e79780fadfd8590d971fed60c6941e
Ricky Costa <79061523+InquestGeronimo@users.noreply.github.com>
2022-03-22 14:57:10 -0400

    small edits to the benchmark readme (#286)
    * smaall edits to the benchmark readme
    
    * make changes to intro paragraph and altered batch size 'Sample CLI' section
    
    * grammar edits
    
    * fixed title
    
    * edited grammar in readme and benchmark_model.py
    
    * forgot to add a file
    
    * Apply suggestions from code review
    
    Co-authored-by: Michael Goin <michael@neuralmagic.com>
    Co-authored-by: Jeannie Finks <74554921+jeanniefinks@users.noreply.github.com>

--- src/deepsparse/benchmark_model/benchmark_model.py
+++ src/deepsparse/benchmark_model/benchmark_model.py
@@ -28,20 +28,20 @@ usage: deepsparse.benchmark [-h] [-b BATCH_SIZE] [-shapes INPUT_SHAPES]
 Benchmark ONNX models in the DeepSparse Engine
 
 positional arguments:
-  model_path            Path to an ONNX model file or SparseZoo model stub
+  model_path            Path to an ONNX model file or SparseZoo model stub.
 
 optional arguments:
-  -h, --help            show this help message and exit
+  -h, --help            show this help message and exit.
   -b BATCH_SIZE, --batch_size BATCH_SIZE
                         The batch size to run the analysis for. Must be
-                        greater than 0
+                        greater than 0.
   -shapes INPUT_SHAPES, --input_shapes INPUT_SHAPES
                         Override the shapes of the inputs, i.e. -shapes
                         "[1,2,3],[4,5,6],[7,8,9]" results in input0=[1,2,3]
-                        input1=[4,5,6] input2=[7,8,9]
+                        input1=[4,5,6] input2=[7,8,9].
   -ncores NUM_CORES, --num_cores NUM_CORES
                         The number of physical cores to run the analysis on,
-                        defaults to all physical cores available on the system
+                        defaults to all physical cores available on the system.
   -s {async,sync,elastic}, --scenario {async,sync,elastic}
                         Choose between using the async, sync and elastic
                         scenarios. Sync and async are similar to the single-
@@ -62,13 +62,13 @@ optional arguments:
   -pin {none,core,numa}, --thread_pinning {none,core,numa}
                         Enable binding threads to cores ('core' the default),
                         threads to cores on sockets ('numa'), or disable
-                        ('none')
+                        ('none').
   -e {deepsparse,onnxruntime}, --engine {deepsparse,onnxruntime}
                         Inference engine backend to run eval on. Choices are
-                        'deepsparse', 'onnxruntime'. Default is 'deepsparse'
-  -q, --quiet           Lower logging verbosity
+                        'deepsparse', 'onnxruntime'. Default is 'deepsparse'.
+  -q, --quiet           Lower logging verbosity.
   -x EXPORT_PATH, --export_path EXPORT_PATH
-                        Store results into a JSON file
+                        Store results into a JSON file.
 
 ##########
 Example on a BERT from SparseZoo:
