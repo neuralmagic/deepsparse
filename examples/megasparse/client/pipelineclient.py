9db5ca8440680385ad8ea13b13e3088c910443b2
Ricky Costa <rcosta84@gmail.com>
2022-04-05 15:43:49 -0400

    small edit to docstring

--- examples/megasparse/client/pipelineclient.py
+++ examples/megasparse/client/pipelineclient.py
@@ -12,10 +12,6 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-"""
-Client object for making requests to the example DeepSparse BERT inference server
-"""
-
 import json
 from typing import List
 
