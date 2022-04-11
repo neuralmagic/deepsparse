684b46fb11f40f839aeac7a1d69d31618fa5ca2f
Ricky Costa <rcosta84@gmail.com>
2022-04-11 11:28:47 -0400

    style changes

--- examples/sparseserver-ui/client/samples.py
+++ examples/sparseserver-ui/client/samples.py
@@ -14,36 +14,42 @@
 
 sample = {
     "example 1": {
-        "context": """
-        The DeepSparse Engine is a CPU runtime that delivers 
-        GPU-class performance by taking advantage of sparsity within neural 
-        networks to reduce compute required as well as accelerate memory bound 
-        workloads. It is focused on model deployment and scaling machine 
-        learning pipelines, fitting seamlessly into your existing deployments 
-        as an inference backend.""",
-        "question": """What does the DeepSparse Engine take advantage of 
-        within neural networks?""",
+        "context": (
+            "The DeepSparse Engine is a CPU runtime that delivers"
+            "GPU-class performance by taking advantage of sparsity within neural"
+            "networks to reduce compute required as well as accelerate memory bound"
+            "workloads. It is focused on model deployment and scaling machine"
+            "learning pipelines, fitting seamlessly into your existing deployments"
+            "as an inference backend."
+        ),
+        "question": (
+            "What does the DeepSparse Engine take advantage of within neural networks?"
+        ),
     },
     "example 2": {
-        "context": """
-        Concerns were raised over whether Levi's Stadium's field was of a high 
-        enough quality to host a Super Bowl; during the inaugural season, the field 
-        had to be re-sodded multiple times due to various issues, and during a week 
-        6 game earlier in the 2015 season, a portion of the turf collapsed under 
-        Baltimore Ravens kicker Justin Tucker, causing him to slip and miss a field 
-        goal.""",
-        "question": "What collapsed on Justin Tucker?",
+        "context": (
+            "Concerns were raised over whether Levi's Stadium's field was of a high"
+            "enough quality to host a Super Bowl; during the inaugural season, the field"
+            "had to be re-sodded multiple times due to various issues, and during a week"
+            "6 game earlier in the 2015 season, a portion of the turf collapsed under"
+            "Baltimore Ravens kicker Justin Tucker, causing him to slip and miss a field"
+            "goal."
+        ),
+        "question": ("What collapsed on Justin Tucker?"),
     },
     "example 3": {
-        "context": """
-        The league announced on October 16, 2012, that the two finalists were Sun 
-        Life Stadium and Levi's Stadium. The South Florida/Miami area has previously 
-        hosted the event 10 times (tied for most with New Orleans), with the 
-        most recent one being Super Bowl XLIV in 2010. The San Francisco Bay 
-        Area last hosted in 1985 (Super Bowl XIX), held at Stanford Stadium in 
-        Stanford, California, won by the home team 49ers. The Miami bid depended 
-        on whether the stadium underwent renovations.""",
-        "question": """What was the most recent Super Bowl that took place at Sun 
-    Life Stadium in Miami?""",
+        "context": (
+            "The league announced on October 16, 2012, that the two finalists were Sun"
+            "Life Stadium and Levi's Stadium. The South Florida/Miami area has previously"
+            "hosted the event 10 times (tied for most with New Orleans), with the"
+            "most recent one being Super Bowl XLIV in 2010. The San Francisco Bay"
+            "Area last hosted in 1985 (Super Bowl XIX), held at Stanford Stadium in"
+            "Stanford, California, won by the home team 49ers. The Miami bid depended"
+            "on whether the stadium underwent renovations."
+        ),
+        "question": (
+            "What was the most recent Super Bowl that took place at Sun "
+            "Life Stadium in Miami?"
+        ),
     },
 }
