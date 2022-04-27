b2ac821546fe41ce29b377e85cc687d5c2cf4929
mgoin <michael@neuralmagic.com>
2022-04-27 09:45:27 -0400

    Skip flake8

--- src/deepsparse/benchmark/__init__.py
+++ src/deepsparse/benchmark/__init__.py
@@ -12,5 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+# flake8: noqa
+
 from .ort_engine import ORTEngine
 from .results import *
