1da66a8f3af693468e63ca7ae724eb831729b5b8
Ricky Costa <rcosta84@gmail.com>
2022-05-18 15:45:13 -0400

    style changes

--- examples/aws-sagemaker/create_endpoint.py
+++ examples/aws-sagemaker/create_endpoint.py
@@ -1,14 +1,27 @@
-import boto3
+# Copyright (c) 2021 - present / Neuralmagic, Inc. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing,
+# software distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 import subprocess
+
+import boto3
 from rich.pretty import pprint
 
 
 class SparseMaker:
-
-
     def __init__(
         self,
-        instance_count,  
+        instance_count,
         region_name,
         instance_type,
         image_name,
@@ -23,9 +36,7 @@ class SparseMaker:
 
         # Docker Image and Repository
         self.image_name = image_name
-        self.build_cmd = [
-            f"docker build -t {self.image_name} ."
-        ]
+        self.build_cmd = [f"docker build -t {self.image_name} ."]
         self.repo_name = repository_name
         self.push_script = image_push_script
 
@@ -55,11 +66,11 @@ class SparseMaker:
         ]
         self.endpoint_config_name = config_name
         self.endpoint_config = {
-            "EndpointConfigName": self.endpoint_config_name, 
-            "ProductionVariants": self.production_variants
+            "EndpointConfigName": self.endpoint_config_name,
+            "ProductionVariants": self.production_variants,
         }
         self.endpoint_name = endpoint_name
-      
+
     def create_image(self):
 
         subprocess.run(self.build_cmd, shell=True)
@@ -96,8 +107,8 @@ class SparseMaker:
     def create_endpoint(self):
 
         self.sm_boto3.create_endpoint(
-            EndpointName=self.endpoint_name, 
-            EndpointConfigName=self.endpoint_config_name
+            EndpointName=self.endpoint_name,
+            EndpointConfigName=self.endpoint_config_name,
         )
 
         pprint(self.sm_boto3.describe_endpoint(EndpointName=self.endpoint_name))
@@ -105,32 +116,34 @@ class SparseMaker:
     def nuke_endpoint(self):
 
         self.sm_boto3.delete_endpoint(EndpointName=self.endpoint_name)
-        self.sm_boto3.delete_endpoint_config(EndpointConfigName=self.endpoint_config_name)
+        self.sm_boto3.delete_endpoint_config(
+            EndpointConfigName=self.endpoint_config_name
+        )
         self.sm_boto3.delete_model(ModelName=self.model_name)
-        
+
         print("endpoint nuked!")
 
 
 if __name__ == "__main__":
 
-        Sage = SparseMaker(
-            instance_count      = 1,
-            region_name         = "us-east-1", 
-            instance_type       = "ml.c5.large",
-            image_name          = "deepsparse-sagemaker-example",
-            repository_name     = "deepsparse-sagemaker",
-            image_push_script   = "./push_image.sh",
-            variant_name        = "QuestionAnsweringDeepSparseDemo", # ^[a-zA-Z0-9](-*[a-zA-Z0-9]){0,62}
-            model_name          = "question-answering-example",   
-            config_name         = "QuestionAnsweringExampleConfig",  # ^[a-zA-Z0-9](-*[a-zA-Z0-9]){0,62}
-            endpoint_name       = "question-answering-example-endpoint",
-            role_arn            = "arn:aws:iam::831168642639:role/ricky",
-        )
+    Sage = SparseMaker(
+        instance_count=1,
+        region_name="us-east-1",
+        instance_type="ml.c5.large",
+        image_name="deepsparse-sagemaker-example",
+        repository_name="deepsparse-sagemaker",
+        image_push_script="./push_image.sh",
+        variant_name="QuestionAnsweringDeepSparseDemo",
+        model_name="question-answering-example",
+        config_name="QuestionAnsweringExampleConfig",
+        endpoint_name="question-answering-example-endpoint",
+        role_arn="arn:aws:iam::831168642639:role/ricky",
+    )
 
-        Sage.create_image()
-        Sage.create_ecr_repo()
-        Sage.push_image()
-        Sage.create_model()
-        Sage.create_endpoint_config()
-        Sage.create_endpoint()
-        Sage.nuke_endpoint()
+    Sage.create_image()
+    Sage.create_ecr_repo()
+    Sage.push_image()
+    Sage.create_model()
+    Sage.create_endpoint_config()
+    Sage.create_endpoint()
+    Sage.nuke_endpoint()
--- examples/aws-sagemaker/inference.py
+++ examples/aws-sagemaker/inference.py
@@ -1,17 +1,30 @@
-import boto3
+# Copyright (c) 2021 - present / Neuralmagic, Inc. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing,
+# software distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 import json
 
-class Endpoint:
+import boto3
 
+
+class Endpoint:
     def __init__(self, region_name, endpoint_name):
 
         self.region_name = region_name
         self.endpoint_name = endpoint_name
         self.content_type = "application/json"
         self.accept = "text/plain"
-        self.client = boto3.client(
-            "sagemaker-runtime", region_name=self.region_name
-        )
+        self.client = boto3.client("sagemaker-runtime", region_name=self.region_name)
 
     def predict(self, question, context):
 
@@ -21,6 +34,7 @@ class Endpoint:
                 context=context,
             )
         )
+
         res = self.client.invoke_endpoint(
             EndpointName=self.endpoint_name,
             Body=body,
