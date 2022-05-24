7afeeecc4181cc21a6a56da80119da98ebc5ebd5
Dickson Neoh <dickson.neoh@gmail.com>
2022-05-24 23:49:55 +0800

    Fix font scale  (#384)
    * add resizable window and escape key to quit
    
    * fix font scale
    
    * remove redundant logger

--- examples/ultralytics-yolo/deepsparse_utils.py
+++ examples/ultralytics-yolo/deepsparse_utils.py
@@ -751,7 +751,7 @@ def annotate_image(
             f"images_per_sec: {int(images_per_sec)}",
             (50, 50),
             cv2.FONT_HERSHEY_SIMPLEX,
-            2.0,  # font scale
+            1.0,  # font scale
             (245, 46, 6),  # color
             2,  # thickness
             cv2.LINE_AA,
