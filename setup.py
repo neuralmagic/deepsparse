1872d3ba9bf1dbd5ed1111afb514a6bd671b6cd6
Rahul Tuli <rahul@neuralmagic.com>
2022-06-08 14:53:33 -0400

    Style

--- setup.py
+++ setup.py
@@ -53,7 +53,7 @@ _deps = [
     "requests>=2.0.0",
     "tqdm>=4.0.0",
     "protobuf>=3.12.2,<4",
-    "click<8.1"
+    "click<8.1",
 ]
 _nm_deps = [f"{'sparsezoo' if is_release else 'sparsezoo-nightly'}~={version_base}"]
 _dev_deps = [
