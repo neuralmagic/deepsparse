f57879cb1b06beea548041e74003c299c02aa3f2
Corey Lowman <corey@neuralmagic.com>
2022-07-28 17:18:03 -0400

    Changing transformers NM_INTEGRATED except to specific exceptions

--- src/deepsparse/transformers/__init__.py
+++ src/deepsparse/transformers/__init__.py
@@ -106,7 +106,7 @@ def _check_transformers_install():
         import transformers as _transformers
 
         assert _transformers.NM_INTEGRATED
-    except Exception:
+    except (AssertionError, ImportError) as e:
         _LOGGER.warning(
             "the neuralmagic fork of transformers may not be installed. it can be "
             "installed via "
