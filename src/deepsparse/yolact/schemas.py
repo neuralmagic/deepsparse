28f5cd20733cb504454c6f5b1ee74f802c15da29
dbogunowicz <97082108+dbogunowicz@users.noreply.github.com>
2022-07-28 16:57:17 +0200

    YOLACT -> deepsparse pipeline + annotation script (#458)
    * Initial Commit
    
    * Add a yolact directory to this Parent Branch
    
    * Add a stubbed out YOLACT Pipeline class + Input/Output Schema classes
    
    * initial commit
    
    * port completed. time to refactor and beautify
    
    * draft of pipeline ready
    
    * observed some weird behavior for big images, investigating
    
    * code complete, time for testing and refactoring
    
    * rebase
    
    * refactoring done, testing
    
    * ready
    
    * Update src/deepsparse/yolo/schemas.py
    
    Co-authored-by: Rahul Tuli <rahul@neuralmagic.com>
    
    * Update src/deepsparse/yolo/schemas.py
    
    Co-authored-by: Rahul Tuli <rahul@neuralmagic.com>
    
    * Update src/deepsparse/yolact/utils/utils.py
    
    Co-authored-by: Rahul Tuli <rahul@neuralmagic.com>
    
    * Update src/deepsparse/yolact/utils/annotate.py
    
    Co-authored-by: Rahul Tuli <rahul@neuralmagic.com>
    
    * Update src/deepsparse/yolact/pipelines.py
    
    Co-authored-by: Rahul Tuli <rahul@neuralmagic.com>
    
    * Update src/deepsparse/yolo/pipelines.py
    
    Co-authored-by: Rahul Tuli <rahul@neuralmagic.com>
    
    * Update src/deepsparse/yolact/utils/annotate.py
    
    Co-authored-by: Rahul Tuli <rahul@neuralmagic.com>
    
    * round of corrections
    
    * final touch
    
    * move bug fix from docs PR to the main YOLO PR
    
    * Add torch.jit preprocessing (#510)
    
    * initial commit
    
    * add profiling
    
    * really profiling man
    
    * start refactoring
    
    * finished refactoring so it seems
    
    * done
    
    * ready to merge
    
    * Update utils.py
    
    Co-authored-by: bogunowicz@arrival.com <bogunowicz@arrival.com>
    
    * add deps
    
    * Added support to autoinstall yolact dependencies (#514)
    
    * Update src/deepsparse/yolact/pipelines.py
    
    Co-authored-by: Konstantin Gulin <66528950+KSGulin@users.noreply.github.com>
    
    * Update utils.py
    
    * Update src/deepsparse/yolact/pipelines.py
    
    Co-authored-by: kylesayrs <kylesayrs@gmail.com>
    
    * Update src/deepsparse/yolact/annotate.py
    
    Co-authored-by: kylesayrs <kylesayrs@gmail.com>
    
    * correction after Kyles comment
    
    Co-authored-by: Rahul Tuli <rahul@neuralmagic.com>
    Co-authored-by: bogunowicz@arrival.com <bogunowicz@arrival.com>
    Co-authored-by: Konstantin Gulin <66528950+KSGulin@users.noreply.github.com>
    Co-authored-by: Benjamin Fineran <bfineran@users.noreply.github.com>
    Co-authored-by: kylesayrs <kylesayrs@gmail.com>

---
+++ src/deepsparse/yolact/schemas.py
@@ -0,0 +1,124 @@
+# Copyright (c) 2021 - present / Neuralmagic, Inc. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing,
+# software distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+"""
+Input/Output Schemas for Image Segmentation with YOLACT
+"""
+
+from collections import namedtuple
+from typing import List, Optional, Union
+
+import numpy
+from pydantic import BaseModel, Field
+
+
+__all__ = [
+    "YOLACTInputSchema",
+    "YOLACTOutputSchema",
+]
+
+_YOLACTImageOutput = namedtuple(
+    "_YOLACTImageOutput", ["classes", "scores", "boxes", "masks"]
+)
+
+
+class YOLACTInputSchema(BaseModel):
+    """
+    Input Model for YOLACT
+    """
+
+    images: Union[str, numpy.ndarray, List[Union[str, numpy.ndarray]]] = Field(
+        description="List of images to process"
+    )
+
+    confidence_threshold: float = Field(
+        default=0.2,
+        description="Confidence threshold applied to the raw detection at "
+        "`detection` step. If a raw detection's score is lower "
+        "than the threshold, it will be automatically discarded",
+    )
+    nms_threshold: float = Field(
+        default=0.5,
+        description="Minimum IoU overlap threshold for a prediction to "
+        "consider it valid (used in Non-Maximum-Suppression step)",
+    )
+    top_k_preprocessing: int = Field(
+        default=100,
+        description="The maximal number of best detections (per class) to be "
+        "kept after the Non-Maximum-Suppression step",
+    )
+    max_num_detections: int = Field(
+        default=100,
+        description="The maximal number of best detections (across all classes) "
+        "to be kept after the Non-Maximum-Suppression step",
+    )
+    score_threshold: float = Field(
+        default=0.0,
+        description="Confidence threshold applied to the raw detection at "
+        "`postprocess` step (optional)",
+    )
+
+    @classmethod
+    def from_files(cls, files: List[str], **kwargs) -> "YOLACTInputSchema":
+        """
+        :param files: list of file paths to create YOLOInput from
+        :param kwargs: extra keyword args to pass to YOLOInput constructor
+        :return: YOLOInput constructed from files
+        """
+        if "images" in kwargs:
+            raise ValueError(
+                f"argument 'images' cannot be specified in {cls.__name__} when "
+                "constructing from file(s)"
+            )
+        return cls(images=files, **kwargs)
+
+    class Config:
+        arbitrary_types_allowed = True
+
+
+class YOLACTOutputSchema(BaseModel):
+    """
+    Output Model for YOLACT
+    """
+
+    classes: List[List[Optional[Union[int, str]]]] = Field(
+        description="List of predictions"
+    )
+    scores: List[List[Optional[float]]] = Field(
+        description="List of scores, one for each prediction"
+    )
+    boxes: List[List[Optional[List[float]]]] = Field(
+        description="List of bounding boxes, one for each prediction"
+    )
+    masks: List[Optional[numpy.ndarray]] = Field(
+        description="List of masks, one for each prediction"
+    )
+
+    class Config:
+        arbitrary_types_allowed = True
+
+    def __getitem__(self, index):
+        if index >= len(self.classes):
+            raise IndexError("Index out of range")
+
+        return _YOLACTImageOutput(
+            self.classes[index],
+            self.scores[index],
+            self.boxes[index],
+            self.masks[index],
+        )
+
+    def __iter__(self):
+        for index in range(len(self.classes)):
+            yield self[index]
