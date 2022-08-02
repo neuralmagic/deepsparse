d3e9dd34d624b94d5cde75a30ab637a784decfa1
kylesayrs <kylesayrs@gmail.com>
2022-08-02 16:04:23 -0400

    Deleted dynamic installation, install from requirements file in setup.py (#532)
    * Deleted dynamic installation, install from requirements file in setup.py
    
    * clearer comment, apply style
    
    * Update tests, remove haystack tests autoinstall

--- setup.py
+++ setup.py
@@ -46,6 +46,14 @@ _PACKAGE_NAME = "deepsparse" if is_release else "deepsparse-nightly"
 # File regexes for binaries to include in package_data
 binary_regexes = ["*/*.so", "*/*.so.*", "*.bin", "*/*.bin"]
 
+
+def _parse_requirements_file(file_path):
+    with open(file_path, "r") as requirements_file:
+        lines = requirements_file.read().splitlines()
+
+    return [line for line in lines if len(line) > 0 and line[0] != "#"]
+
+
 _deps = [
     "numpy>=1.16.3",
     "onnx>=1.5.0,<=1.12.0",
@@ -87,11 +95,26 @@ _server_deps = [
 _onnxruntime_deps = [
     "onnxruntime>=1.7.0",
 ]
-
 _yolo_integration_deps = [
     "torchvision>=0.3.0,<=0.10.1",
     "opencv-python",
 ]
+
+# haystack dependencies are installed from a requirements file to avoid
+# conflicting versions with NM's deepsparse/transformers
+_haystack_requirements_file_path = os.path.join(
+    os.path.dirname(os.path.realpath(__file__)),
+    "src",
+    "deepsparse",
+    "transformers",
+    "haystack",
+    "haystack_reqs.txt",
+)
+_haystack_integration_deps = _parse_requirements_file(
+    _haystack_requirements_file_path
+) + [
+    "farm-haystack[all]==1.4.0"
+]  # install farm-haystack last
 
 
 def _check_supported_system():
@@ -196,6 +219,7 @@ def _setup_extras() -> Dict:
         "server": _server_deps,
         "onnxruntime": _onnxruntime_deps,
         "yolo": _yolo_integration_deps,
+        "haystack": _haystack_integration_deps,
     }
 
 
