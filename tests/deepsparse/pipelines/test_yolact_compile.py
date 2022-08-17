8d12d0b5bec2baa6a39e49b0e44d1fe232106e83
Corey Lowman <corey@neuralmagic.com>
2022-08-17 09:42:49 -0400

    Adding test for dynamic batch yolact

--- tests/deepsparse/pipelines/test_yolact_compile.py
+++ tests/deepsparse/pipelines/test_yolact_compile.py
@@ -1,4 +1,5 @@
 from deepsparse import Pipeline
+from concurrent.futures import ThreadPoolExecutor
 
 
 def test_compile_yolact():
@@ -6,3 +7,12 @@ def test_compile_yolact():
         "zoo:cv/segmentation/yolact-darknet53/pytorch/dbolya/coco/pruned82_quant-none"
     )
     Pipeline.create("yolact", model_path=stub)
+
+
+def test_compile_yolact_dynamic_batch():
+    stub = (
+        "zoo:cv/segmentation/yolact-darknet53/pytorch/dbolya/coco/pruned82_quant-none"
+    )
+    Pipeline.create(
+        "yolact", model_path=stub, batch_size=None, executor=ThreadPoolExecutor()
+    )
