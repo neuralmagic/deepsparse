5487bc4d1f142c3390986ca1c8ee8e98663bcca4
Rahul Tuli <rahul@neuralmagic.com>
2022-08-23 11:16:01 -0400

    Remove click versions from everywhere except `setup.py`

--- src/deepsparse/image_classification/__init__.py
+++ src/deepsparse/image_classification/__init__.py
@@ -26,7 +26,7 @@ _Dependency = namedtuple("_Dependency", ["name", "version", "necessary"])
 def _auto_install_dependencies():
     dependencies = [
         _Dependency(name="torchvision", version=">=0.3.0,<=0.12.0", necessary=True),
-        _Dependency(name="click", version="<8.1", necessary=False),
+        _Dependency(name="click", version="", necessary=False),
     ]
 
     for dependency in dependencies:
