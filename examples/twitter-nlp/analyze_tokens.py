f85374d778aafb540d2d99e059d6130c984266b8
Michael Goin <michael@neuralmagic.com>
2022-08-30 18:38:40 -0400

    Twitter NER important token extraction example (#605)
    * Twitter NER location analysis
    
    * Cleanup, add engine and important token
    
    * Style
    
    * Documentation
    
    * Remove wildfire scrape
    
    * Update requirements.txt
    
    * Update setup.py
    
    * Add --engine to analyze_sentiment
    
    * Add tests
    
    * Fix test
    
    * quality
    
    * Add deps install
    
    * time.perf_counter

--- examples/twitter-nlp/analyze_tokens.py
+++ examples/twitter-nlp/analyze_tokens.py
@@ -15,46 +15,44 @@
 # flake8: noqa
 
 """
-Script to analyze the sentiment of a given file of tweets from Twitter
+Script to analyze the tokens of a given file of tweets from Twitter
 in batch processing mode.
 
 ##########
 Command help:
-Usage: analyze_sentiment.py [OPTIONS]
+Usage: analyze_tokens.py [OPTIONS]
 
-  Analyze the sentiment of the tweets given in the tweets_file and print out
+  Analyze the tokens of the tweets given in the tweets_file and print out
   the results.
 
 Options:
-  --model_path TEXT               The path to the sentiment analysis model to
+  --model_path TEXT               The path to the token classification model to
                                   load. Either a model.onnx file, a model
                                   folder containing the model.onnx and
                                   supporting files, or a SparseZoo model stub.
   --tweets_file TEXT              The path to the tweets json txt file to
-                                  analyze sentiment for.
+                                  analyze tokens for.
   --batch_size INTEGER            The batch size to process the tweets with. A
                                   higher batch size may increase performance
                                   at the expense of memory resources and
                                   individual latency.
   --total_tweets INTEGER          The total number of tweets to analyze from
-                                  the tweets_file. Defaults to None which will
+                                  the tweets_file.Defaults to None which will
                                   run through all tweets contained in the
                                   file.
   --engine [deepsparse|onnxruntime]
                                   Inference engine to use. Default is
                                   deepsparse
+  --important_token [MIS|PER|ORG|LOC]
+                                  Which tokens to extract: 'MIS' for
+                                  miscellaneous, 'PER' for people, 'ORG' for
+                                  organizations, 'LOC' for locations
   --help                          Show this message and exit.
 
 ##########
-Example running a sparse, quantized sentiment analysis model:
-python analyze_sentiment.py
-    --model_path "zoo:nlp/sentiment_analysis/bert-base/pytorch/huggingface/sst2/12layer_pruned80_quant-none-vnni"
-    --tweets_file /PATH/TO/OUTPUT/FROM/scrape.py
-
-##########
-Example running a dense, unoptimized sentiment analysis model:
-python analyze_sentiment.py
-    --model_path "zoo:nlp/sentiment_analysis/bert-base/pytorch/huggingface/sst2/base-none"
+Example running a sparse, quantized token classification model:
+python analyze_tokens.py
+    --model_path "zoo:nlp/token_classification/distilbert-none/pytorch/huggingface/conll2003/pruned80_quant-none-vnni"
     --tweets_file /PATH/TO/OUTPUT/FROM/scrape.py
 """
 
@@ -67,6 +65,19 @@ import click
 
 from deepsparse import Pipeline
 from rich import print
+
+
+ner_tag_map = {
+    "O": 0,
+    "B-PER": 1,
+    "I-PER": 2,
+    "B-ORG": 3,
+    "I-ORG": 4,
+    "B-LOC": 5,
+    "I-LOC": 6,
+    "B-MISC": 7,
+    "I-MISC": 8,
+}
 
 
 def _load_tweets(tweets_file: str):
@@ -95,28 +106,47 @@ def _batched_model_input(tweets: List[str], batch_size: int) -> Optional[List[st
     return batched
 
 
-def _classified_positive(sentiment: str):
-    return sentiment == "LABEL_1" or sentiment == "positive"
+def _extract_important_tokens(tokens: List, important_token: str):
+    # Note: sometimes a PER/LOC/ORG string can start with a MISC token, we currently miss these
+    loc_tokens = [token for token in tokens if important_token in token.entity]
+
+    loc_words = [token.word for token in loc_tokens]
+    compressed_loc_words = []
+    for word in loc_words:
+        if not word.startswith("##") or not compressed_loc_words:
+            compressed_loc_words.append(word)
+        else:
+            compressed_loc_words[-1] += word[2:]
+    return loc_tokens, compressed_loc_words
 
 
-def _display_results(batch, sentiments):
-    for text, sentiment in zip(batch, sentiments):
-        color = "green" if _classified_positive(sentiment) else "magenta"
-        print(f"[{color}]{text}[/{color}]")
+def _display_results(batch, batch_pred, important_token: str):
+    for text, tokens in zip(batch, batch_pred):
+        loc_tokens, cr_loc_words = _extract_important_tokens(tokens, important_token)
+
+        print()
+        print(text)
+
+        if len(cr_loc_words) > 0:
+            color = "magenta"
+            print(
+                f"Found {len(cr_loc_words)} important tokens: [{color}]{cr_loc_words}[/{color}]"
+            )
+            print(loc_tokens)
 
 
 @click.command()
 @click.option(
     "--model_path",
     type=str,
-    help="The path to the sentiment analysis model to load. "
+    help="The path to the tokens analysis model to load. "
     "Either a model.onnx file, a model folder containing the model.onnx "
     "and supporting files, or a SparseZoo model stub.",
 )
 @click.option(
     "--tweets_file",
     type=str,
-    help="The path to the tweets json txt file to analyze sentiment for.",
+    help="The path to the tweets json txt file to analyze tokens for.",
 )
 @click.option(
     "--batch_size",
@@ -139,23 +169,37 @@ def _display_results(batch, sentiments):
     default="deepsparse",
     help="Inference engine to use. Default is deepsparse",
 )
-def analyze_tweets_sentiment(
-    model_path: str, tweets_file: str, batch_size: int, total_tweets: int, engine: str
+@click.option(
+    "--important_token",
+    type=click.Choice(["MIS", "PER", "ORG", "LOC"]),
+    default="LOC",
+    help="Which tokens to extract: "
+    "'MIS' for miscellaneous, 'PER' for people, "
+    "'ORG' for organizations, 'LOC' for locations",
+)
+def analyze_tweets_tokens(
+    model_path: str,
+    tweets_file: str,
+    batch_size: int,
+    total_tweets: int,
+    engine: str,
+    important_token: str,
 ):
     """
-    Analyze the sentiment of the tweets given in the tweets_file and
+    Analyze the tokens of the tweets given in the tweets_file and
     print out the results.
     """
     print("Loading the model for inference...")
-    text_pipeline = Pipeline.create(
-        task="text-classification",
+    token_classify = Pipeline.create(
+        task="token-classification",
         model_path=model_path,
         batch_size=batch_size,
         engine_type=engine,
     )
+
     tweets = _load_tweets(tweets_file)
     tweets = _prep_data(tweets, total_tweets)
-    tot_sentiments = []
+    tot_tokens = []
     times = []
 
     while True:
@@ -163,43 +207,24 @@ def analyze_tweets_sentiment(
         if batch is None:
             break
         start = time.perf_counter()
-        sentiments = text_pipeline(batch)
+        tokens = token_classify(inputs=batch)
         end = time.perf_counter()
 
-        if sentiments.__class__.__name__ == "TextClassificationOutput":
-            sentiments: List[str] = sentiments.labels
-        else:
-            # backwards compatibility with old pipelines
-            sentiments: List[str] = [sentiment["label"] for sentiment in sentiments]
+        tokens = tokens.predictions
 
-        _display_results(batch, sentiments)
-        tot_sentiments.extend(sentiments)
+        _display_results(batch, tokens, important_token)
+        tot_tokens.extend(tokens)
         times.append(end - start)
 
-    num_positive = sum(
-        [1 if _classified_positive(sent) else 0 for sent in tot_sentiments]
-    )
-    num_negative = sum(
-        [1 if not _classified_positive(sent) else 0 for sent in tot_sentiments]
-    )
+    num_tokens = len([item for sublist in tot_tokens for item in sublist])
     print("\n\n\n")
     print("###########################################################################")
-    print(f"Completed analyzing {len(tot_sentiments)} tweets for sentiment,")
-
-    if num_positive >= num_negative:
-        print(
-            f"[green]General sentiment is positive with "
-            f"{100*num_positive/float(len(tot_sentiments)):.0f}% in favor.[/green]"
-        )
-    else:
-
-        print(
-            f"[magenta]General sentiment is negative with "
-            f"{100*num_negative/float(len(tot_sentiments)):.0f}% against.[/magenta]"
-        )
+    print(
+        f"Completed analyzing {len(tot_tokens)} tweets for a total of {num_tokens} tokens extracted."
+    )
     print(f"This took {sum(times):2f} seconds total")
     print("###########################################################################")
 
 
 if __name__ == "__main__":
-    analyze_tweets_sentiment()
+    analyze_tweets_tokens()
