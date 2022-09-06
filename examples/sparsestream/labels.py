afd9903542e907a1a96be3f4be3039db37c24a06
Ricky Costa <rcosta84@gmail.com>
2022-09-06 10:29:53 -0400

    edited labels and config files

--- examples/sparsestream/labels.py
+++ examples/sparsestream/labels.py
@@ -12,27 +12,27 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-sentiments = {"LABEL_0": "Bearish", "LABEL_1": "Bullish", "LABEL_2": "Neutral"}
+sentiments = {"0": "Bearish", "1": "Bullish", "2": "Neutral"}
 
 topics = {
-    "LABEL_0": "Analyst Update",
-    "LABEL_1": "Fed | Central Banks",
-    "LABEL_2": "Company | Product News",
-    "LABEL_3": "Treasuries | Corporate Debt",
-    "LABEL_4": "Dividend",
-    "LABEL_5": "Earnings",
-    "LABEL_6": "Energy | Oil",
-    "LABEL_7": "Financials",
-    "LABEL_8": "Currencies",
-    "LABEL_9": "General News | Opinion",
-    "LABEL_10": "Gold | Metals | Materials",
-    "LABEL_11": "IPO",
-    "LABEL_12": "Legal | Regulation",
-    "LABEL_13": "M&A | Investments",
-    "LABEL_14": "Macro",
-    "LABEL_15": "Markets",
-    "LABEL_16": "Politics",
-    "LABEL_17": "Personnel Change",
-    "LABEL_18": "Stock Commentary",
-    "LABEL_19": "Stock Movement",
+    "0": "Analyst Update",
+    "1": "Fed | Central Banks",
+    "2": "Company | Product News",
+    "3": "Treasuries | Corporate Debt",
+    "4": "Dividend",
+    "5": "Earnings",
+    "6": "Energy | Oil",
+    "7": "Financials",
+    "8": "Currencies",
+    "9": "General News | Opinion",
+    "10": "Gold | Metals | Materials",
+    "11": "IPO",
+    "12": "Legal | Regulation",
+    "13": "M&A | Investments",
+    "14": "Macro",
+    "15": "Markets",
+    "16": "Politics",
+    "17": "Personnel Change",
+    "18": "Stock Commentary",
+    "19": "Stock Movement",
 }
