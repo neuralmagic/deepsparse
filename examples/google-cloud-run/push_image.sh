6b68cec0866dd5bedd265f661c1998d49632e4de
Ricky Costa <rcosta84@gmail.com>
2022-09-22 11:35:14 -0400

    dockerfile fix

--- examples/google-cloud-run/push_image.sh
+++ examples/google-cloud-run/push_image.sh
@@ -2,5 +2,5 @@
 
 docker build -t sparserun .
 docker run -n sparse-cloud -p 80:80 sparserun
-docker build . -t gcr.io/sparse-cloud/sparserun:latest
-docker push gcr.io/sparse-cloud/sparserun:latest
+# docker build . -t gcr.io/sparse-cloud/sparserun:latest
+# docker push gcr.io/sparse-cloud/sparserun:latest
