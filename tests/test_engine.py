f2b192c301262ca52c61a51191c86f608a1752d2
Corey Lowman <corey@neuralmagic.com>
2022-09-28 14:34:55 +0000

    Removing unused import

--- tests/test_engine.py
+++ tests/test_engine.py
@@ -12,8 +12,6 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-import os
-
 import pytest
 from deepsparse import Engine, compile_model
 from deepsparse.utils import verify_outputs
