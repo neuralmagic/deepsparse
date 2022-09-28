5ed0f812e3b8d351df4fb84a56e40506396f20c7
Corey Lowman <corey@neuralmagic.com>
2022-09-28 14:33:59 +0000

    Fixing quality

--- tests/test_engine.py
+++ tests/test_engine.py
@@ -12,8 +12,9 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-import pytest
 import os
+
+import pytest
 from deepsparse import Engine, compile_model
 from deepsparse.utils import verify_outputs
 from sparsezoo import Model
