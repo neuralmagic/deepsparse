8136f5c6f1fb77476280fcb0b78ece29e8ac98c7
dbogunowicz <97082108+dbogunowicz@users.noreply.github.com>
2022-10-11 16:09:37 +0200

    initial commit

---
+++ prometheus_tutorial_1/client/client.py
@@ -0,0 +1,15 @@
+import requests
+import sys
+from time import sleep
+
+def run_inference(image, port):
+    url = f'http://0.0.0.0:{port}/predict/from_files'
+    path = [image]
+    while True:
+        sleep(2)
+        files = [('request', open(img, 'rb')) for img in path]
+        resp = requests.post(url=url, files=files)
+
+if  __name__ == "__main__":
+    run_inference(image=sys.argv[1], port=sys.argv[2])
+
