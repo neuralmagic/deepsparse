692fe257ef11acf46f5b8a9044008b6ec89493d6
bogunowicz@arrival.com <bogunowicz@arrival.com>
2022-10-12 08:22:12 +0200

    ready for review

--- tutorial/client/client.py
+++ tutorial/client/client.py
@@ -1,6 +1,8 @@
 import requests
 import sys
+
 from time import sleep
+
 
 def run_inference(image, port):
     url = f'http://0.0.0.0:{port}/predict/from_files'
@@ -8,8 +10,8 @@ def run_inference(image, port):
     while True:
         sleep(2)
         files = [('request', open(img, 'rb')) for img in path]
-        resp = requests.post(url=url, files=files)
+        requests.post(url=url, files=files)
 
-if  __name__ == "__main__":
+
+if __name__ == "__main__":
     run_inference(image=sys.argv[1], port=sys.argv[2])
-
