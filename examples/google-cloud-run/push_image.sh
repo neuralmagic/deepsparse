2fb25b11054a9e7ee2080d8b44fb7f2dc5aed064
Ricky Costa <rcosta84@gmail.com>
2022-10-13 11:22:03 -0400

    add config

--- examples/google-cloud-run/push_image.sh
+++ examples/google-cloud-run/push_image.sh
@@ -1,6 +1,17 @@
 #!/bin/bash
 
+billingid=019B6C-2EF10D-D5D4E2
+projectid=deepsparse-cloud-1
+gcloud projects create $projectid --name="Cloud Run example"
+gcloud config set project $projectid
+gcloud alpha billing accounts projects link $projectid --billing-account $billingid
+gcloud services enable containerregistry.googleapis.com
+
 docker build -t sparserun .
-docker run -n sparse-cloud -p 80:80 sparserun
-# docker build . -t gcr.io/sparse-cloud/sparserun:latest
-# docker push gcr.io/sparse-cloud/sparserun:latest
+# docker run -d --name sparse-cloud -p 80:80 sparserun 
+docker build . -t gcr.io/${projectid}/sparserun:latest
+docker push gcr.io/${projectid}/sparserun:latest
+
+
+# gcloud container images delete gcr.io/${project}/quickstart-image:tag1 --force-delete-tags
+
