11d4726e7e01717a99a13b597cb17310bf15e536
bogunowicz@arrival.com <bogunowicz@arrival.com>
2022-10-24 16:01:39 +0200

    fix annotations

--- tests/deepsparse/loggers/test_function_logger.py
+++ tests/deepsparse/loggers/test_function_logger.py
@@ -16,6 +16,7 @@ import pytest
 from deepsparse import FunctionLogger, Pipeline, PythonLogger
 from tests.utils import mock_engine
 
+
 CONFIG_1 = {
     "pipeline_inputs": [
         {"function": "identity", "target_logger": "python", "frequency": 3}
@@ -36,18 +37,18 @@ CONFIG_3 = {
     ],
     "pipeline_outputs": [{"function": "identity", "frequency": 4}],
 }
+"""
+if for some target (e.g. "pipeline_inputs") we have
+"frequency" = 3 and
+"num_iterations" = 14
+logging will occur on iterations
+0, 3, 6, 9, 12 -> and thus 5 logs collected
+"""
 
 
 @pytest.mark.parametrize(
     "config,num_iterations,expected_logs_count",
     [
-        """
-        if for some target (e.g. "pipeline_inputs") we have
-        "frequency" = 3 and
-        "num_iterations" = 14
-        logging will occur on iterations
-        0, 3, 6, 9, 12 -> and thus 5 logs collected
-        """
         (CONFIG_1, 14, {"pipeline_inputs": 5}),
         (CONFIG_2, 14, {"pipeline_inputs": 8}),
         (CONFIG_3, 14, {"pipeline_inputs": 8, "pipeline_outputs": 4}),
@@ -67,7 +68,13 @@ def test_function_logger(engine, config, num_iterations, expected_logs_count, ca
         all_messages += [message for message in capsys.readouterr().out.split("\n")]
     for target_name, logs_count in expected_logs_count.items():
         assert (
-            len([m for m in all_messages if (target_name in m) and ("Category: data" in m)])
+            len(
+                [
+                    m
+                    for m in all_messages
+                    if (target_name in m) and ("Category: data" in m)
+                ]
+            )
             == logs_count
         )
     assert (
