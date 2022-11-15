2519efaff2b687e331c7869eb6f72a62be1587d8
mgoin <michael@neuralmagic.com>
2022-11-15 18:25:08 -0500

    Add DeepSparseReader

--- integrations/haystack/tests/test_smoke.py
+++ integrations/haystack/tests/test_smoke.py
@@ -16,10 +16,16 @@ import subprocess
 import sys
 
 import pytest
-from deepsparse.transformers.haystack import DeepSparseEmbeddingRetriever
+from deepsparse.transformers.haystack import (
+    DeepSparseEmbeddingRetriever,
+    DeepSparseReader,
+)
 
 from haystack.document_stores import InMemoryDocumentStore  # isort:skip
-from haystack.pipelines import DocumentSearchPipeline  # isort:skip
+from haystack.pipelines import (
+    DocumentSearchPipeline,
+    ExtractiveQAPipeline,
+)  # isort:skip
 
 
 def run_command(command):
@@ -61,7 +67,7 @@ def install_reqs():
 
 
 @pytest.mark.smoke
-def test_embedding_retriever():
+def test_document_search():
 
     document_store = InMemoryDocumentStore(
         similarity="cosine", embedding_dim=768, use_gpu=False
@@ -96,5 +102,51 @@ def test_embedding_retriever():
     pipeline = DocumentSearchPipeline(retriever)
     results = pipeline.run(
         query="Where does my lover stand?", params={"Retriever": {"top_k": 1}}
+    )
+    print(results)
+
+
+@pytest.mark.smoke
+def test_extractive_qa():
+
+    document_store = InMemoryDocumentStore(
+        similarity="cosine", embedding_dim=768, use_gpu=False
+    )
+    document_store.write_documents(
+        [
+            {
+                "content": "He came on a summer's day "
+                "Bringin' gifts from far away."
+                "But he made it clear he couldn't stay."
+                "No harbor was his home."
+            },
+            {
+                "content": "Somewhere beyond the sea."
+                "Somewhere waiting for me."
+                "My lover stands on golden sands "
+                "And watches the ships that go sailin'"
+            },
+        ]
+    )
+
+    retriever = DeepSparseEmbeddingRetriever(
+        document_store,
+        (
+            "zoo:nlp/masked_language_modeling/distilbert-none/pytorch/huggingface"
+            "/wikipedia_bookcorpus/pruned80_quant-none-vnni"
+        ),
+        pooling_strategy="reduce_mean",
+    )
+    document_store.update_embeddings(retriever)
+
+    reader = DeepSparseReader(
+        model_path="zoo:nlp/question_answering/distilbert-none/pytorch/huggingface/squad/pruned80_quant-none-vnni"
+    )
+
+    pipeline = ExtractiveQAPipeline(reader=reader, retriever=retriever)
+
+    results = pipeline.run(
+        query="Where does my lover stand?",
+        params={"Retriever": {"top_k": 1}, "Reader": {"top_k": 1}},
     )
     print(results)
