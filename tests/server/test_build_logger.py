b200ba66daa99c7c40e6caefddeb10f881322410
Benjamin Fineran <bfineran@users.noreply.github.com>
2022-11-15 10:43:17 -0500

    [data-logging] AsyncLogger implementation (#766)

--- tests/server/test_build_logger.py
+++ tests/server/test_build_logger.py
@@ -15,7 +15,8 @@
 import yaml
 
 import pytest
-from deepsparse.loggers import MetricCategories
+
+from deepsparse.loggers import AsyncLogger, MetricCategories, MultiLogger
 from deepsparse.server.build_logger import build_logger
 from deepsparse.server.config import ServerConfig
 from tests.helpers import find_free_port
@@ -167,9 +168,15 @@ def test_build_logger(yaml_config, raises_error, returns_logger, num_function_lo
     assert bool(logger) == returns_logger
     if not returns_logger:
         return
-    assert len(logger.loggers) == num_function_loggers + 1
+
+    assert isinstance(logger, AsyncLogger)
+    assert isinstance(logger.logger, MultiLogger)
+    assert len(logger.logger.loggers) == num_function_loggers
+
+    assert len(logger.logger.loggers) == num_function_loggers + 1
+
     # check for system logger
-    system_logger = logger.loggers[-1]
+    system_logger = logger.logger.loggers[-1]
     assert system_logger.target_identifier == (
         f"category:{MetricCategories.SYSTEM.value}"
     )
