d2b4d116bb4e163c3b362444654185e738da4e0d
dbogunowicz <97082108+dbogunowicz@users.noreply.github.com>
2022-12-21 19:16:33 +0100

    [Cherry Pick] Pin black version (#836)

--- setup.py
+++ setup.py
@@ -96,7 +96,7 @@ _deps = [
 _nm_deps = [f"{'sparsezoo' if is_release else 'sparsezoo-nightly'}~={version_base}"]
 _dev_deps = [
     "beautifulsoup4>=4.9.3",
-    "black>=20.8b1",
+    "black==22.12.0",
     "flake8>=3.8.3",
     "isort>=5.7.0",
     "m2r2~=0.2.7",
