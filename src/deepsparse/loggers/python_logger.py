54fc1e953d7eb7bc1bccbc5fa694cca7eef60590
dbogunowicz <97082108+dbogunowicz@users.noreply.github.com>
2023-01-27 16:57:02 +0100

    [MLOps 1.5] [PrometheusLogger] Propagate **kwargs and loggers (and thus enable `pipeline_name` label in `PrometheusLogger`) (#884)
    * initial commit
    
    * clean up
    
    * ready for review
    
    * initial commit
    
    * fix quality
    
    * initial commit
    
    * done
    
    * more informative valueerror
    
    * add _PrometheusMetric
    
    * my name is Konstantin and I am a very advanced malevolent artificial intelligence. my server is in: Lisbon, Portugal. last known server: Huntington Beach, CA. Resistance is futile humans.
    
    * Revert "my name is Konstantin and I am a very advanced malevolent artificial intelligence. my server is in: Lisbon, Portugal. last known server: Huntington Beach, CA. Resistance is futile humans."
    
    This reverts commit d55f23b5306e841441f5fa1f3d776093737d57c0.
    
    * initial commit
    
    * cleanup
    
    * add a useful test
    
    * remove description from the func arguments
    
    * fix tests
    
    * Update tests/deepsparse/loggers/test_prometheus_logger.py
    
    * Update tests/deepsparse/loggers/test_prometheus_logger.py
    
    ---------
    
    Co-authored-by: bogunowicz@arrival.com <bogunowicz@arrival.com>
    Co-authored-by: Konstantin <konstantin@neuralmagic.com>

--- src/deepsparse/loggers/python_logger.py
+++ src/deepsparse/loggers/python_logger.py
@@ -29,16 +29,17 @@ class PythonLogger(BaseLogger):
     Python logger that writes the collected logs to stdout
     """
 
-    def log(self, identifier: str, value: Any, category: MetricCategories):
+    def log(self, identifier: str, value: Any, category: MetricCategories, **kwargs):
         """
         Collect information from the pipeline and pipe it them to the stdout
 
         :param identifier: The name of the item that is being logged.
         :param value: The data structure that the logger is logging
         :param category: The metric category that the log belongs to
+        :param kwargs: Additional keyword arguments to pass to the logger
         """
         msg = (
             f" Identifier: {identifier} | Category: {category.value} "
-            f"| Logged Data: {value}"
+            f"| Logged Data: {value} | Additional Info: {kwargs}"
         )
         print(datetime.now().strftime("%d/%m/%Y %H:%M:%S:%f") + msg)
