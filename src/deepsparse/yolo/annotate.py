18b554145a519b3adb9c5eb160e2b1a554745378
Rahul Tuli <rahul@neuralmagic.com>
2023-02-08 07:54:57 -0500

    Update: YOLO pipeline to return scaled Boxes (#881)
    * Update: YOLO pipeline to return scaled Boxes
    
    * Apply: comments from @dbogunowicz 's code review
    
    * Apply: comments from @dbogunowicz, update original image shape extraction logic to work with both 3 and 4 dimensional inputs
    
    * Apply: comments from @dbogunowicz
    
    * Update: YOLO Annotation flow to work with rescaled bounding boxes (#882)
    
    * Update: YOLO pipeline to return scaled Boxes
    
    * Apply: comments from @dbogunowicz 's code review
    
    * Apply: comments from @dbogunowicz, update original image shape extraction logic to work with both 3 and 4 dimensional inputs
    
    * Update: All loaders to only resize an input image if a truthy image_size is passed
    Update: `annotate_image` signature to not use `model_input_size` as yolo-pipeline take care of scaling now
    Remove: `--image-shape` argument as it's actually the expected model input shape Which can be inferred from the graph
    
    * Update: Yolo Annotation to allow shape overrides (#883)
    
    * Update: Annotation script to allow shape override of input onnx model, by adding a `--model_input_shape` CLI argument
    
    * Apply: Comments from @bfineran, update `--model-input-shape` to `--model-input-image-shape`
    
    * Fix: Quality Failures

--- src/deepsparse/yolo/annotate.py
+++ src/deepsparse/yolo/annotate.py
@@ -23,16 +23,16 @@ Options:
                                   used for annotation  [default: zoo:cv/detect
                                   ion/yolov5-s/pytorch/ultralytics/coco/pruned
                                   -aggressive_96]
-  --source TEXT                   File path to an image or directory of image
+  --source TEXT                   File path to image or directory of .jpg
                                   files, a .mp4 video, or an integer (i.e. 0)
                                   for webcam  [required]
   --engine [deepsparse|onnxruntime|torch]
                                   Inference engine backend to run on. Choices
                                   are 'deepsparse', 'onnxruntime', and
                                   'torch'. Default is 'deepsparse'
-  --image_shape, --image_shape INTEGER
-                                  Image shape to use for inference, must be
-                                  two integers  [default: 640, 640]
+  --model_input_image_shape, --model-input-shape INTEGER...
+                                  Image shape to override model with for
+                                  inference, must be two integers
   --num_cores, --num-cores INTEGER
                                   The number of physical cores to run the
                                   annotations with, defaults to using all
@@ -54,8 +54,7 @@ Options:
   --no_save, --no-save            Set flag when source is from webcam to not
                                   save results.Not supported for non-webcam
                                   sources  [default: False]
-  --help                          Show this message and exit
-
+  --help                          Show this message and exit.
 #######
 Examples:
 
@@ -65,7 +64,7 @@ Examples:
 4) deepsparse.object_detection.annotate --source PATH/TO/IMAGE_DIR
 """
 import logging
-from typing import Optional
+from typing import Optional, Tuple
 
 import click
 
@@ -115,12 +114,12 @@ _LOGGER = logging.getLogger(__name__)
     "'onnxruntime', and 'torch'. Default is 'deepsparse'",
 )
 @click.option(
-    "--image_shape",
-    "--image-shape",
+    "--model_input_image_shape",
+    "--model-input-image-shape",
     type=int,
     nargs=2,
-    default=(640, 640),
-    help="Image shape to use for inference, must be two integers",
+    default=None,
+    help="Image shape to override model with for inference, must be two integers",
     show_default=True,
 )
 @click.option(
@@ -173,12 +172,12 @@ def main(
     model_filepath: str,
     source: str,
     engine: str,
-    image_shape: tuple,
     num_cores: Optional[int],
     save_dir: str,
     name: Optional[str],
     target_fps: Optional[float],
     no_save: bool,
+    model_input_image_shape: Optional[Tuple[int, ...]],
 ) -> None:
     """
     Annotation Script for YOLO with DeepSparse
@@ -192,7 +191,7 @@ def main(
     loader, saver, is_video = get_image_loader_and_saver(
         path=source,
         save_dir=save_dir,
-        image_shape=image_shape,
+        image_shape=None,
         target_fps=target_fps,
         no_save=no_save,
     )
@@ -204,6 +203,7 @@ def main(
         class_names="coco",
         engine_type=engine,
         num_cores=num_cores,
+        image_size=model_input_image_shape,
     )
 
     for iteration, (input_image, source_image) in enumerate(loader):
@@ -216,7 +216,6 @@ def main(
             target_fps=target_fps,
             calc_fps=is_video,
             original_image=source_image,
-            model_input_size=image_shape,
         )
 
         if is_webcam:
