606af80f26c0c835c36921832137a3d11783a105
Damian <damian@neuralmagic.com>
2023-04-13 16:11:07 +0000

    make save_onnx more robust

--- src/deepsparse/utils/onnx.py
+++ src/deepsparse/utils/onnx.py
@@ -68,17 +68,32 @@ ONNX_TENSOR_TYPE_MAP = {
 }
 
 
-def save_onnx(model: Model, model_path: str, external_data_file: str) -> bool:
+def save_onnx(
+    model: Model, model_path: str, external_data_file: Optional[str] = None
+) -> bool:
     """
-    Save model to the given path. If the model has external data, store the
-    external data in 'external_data_file'.
-    Returns False if the model had no external data, True otherwise.
+    Save model to the given path.
+    If the model's size is larger than the maximum protobuf size it will be saved
+    with external data. If 'external_data_file' is not specified, it will be created
+    from the name of the model (e.g. 'model.onnx' -> 'model.data').
+    If the model's size is smaller than the maximum protobuf size and the user
+    nevertheless specifies 'external_data_file', the model will be
+    saved with external data.
+
+    :param model: The model to save.
+    :param model_path: The path to save the model to.
+    :param external_data_file: The optional name save the external data to.
+    :return True if the model was saved with external data, False otherwise.
     """
-    if model.ByteSize() < onnx.checker.MAXIMUM_PROTOBUF:
-        onnx.save(model, model_path)
-        return False
-    else:
-        onnx.save_model(
+    if model.ByteSize() > onnx.checker.MAXIMUM_PROTOBUF:
+        external_data_file = (
+            external_data_file or os.path.basename(model_path).replace("onnx", "data"),
+        )
+        _LOGGER.info(
+            "The ONNX model is too large to be saved as a single protobuf. "
+            f"Saving with external data: {external_data_file}"
+        )
+        onnx.save(
             model,
             model_path,
             save_as_external_data=True,
@@ -86,6 +101,22 @@ def save_onnx(model: Model, model_path: str, external_data_file: str) -> bool:
             location=external_data_file,
         )
         return True
+    else:
+        if external_data_file:
+            onnx.save_model(
+                model,
+                model_path,
+                save_as_external_data=True,
+                all_tensors_to_one_file=True,
+                location=external_data_file,
+            )
+        _LOGGER.info(
+            f"Saving the ONNX model {model_path} "
+            f"with external data to {external_data_file}"
+        )
+        return True
+    onnx.save_model(model, model_path)
+    return False
 
 
 @contextlib.contextmanager
