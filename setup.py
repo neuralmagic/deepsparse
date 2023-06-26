3f98e2c912eb28dca110516917f3de0cbedaf8c9
Michael Goin <michael@neuralmagic.com>
2023-06-26 17:12:33 -0400

    Lift protobuf restriction to allow onnx 1.14

--- setup.py
+++ setup.py
@@ -90,7 +90,7 @@ _deps = [
     "pydantic>=1.8.2",
     "requests>=2.0.0",
     "tqdm>=4.0.0",
-    "protobuf>=3.12.2,<=3.20.1",
+    "protobuf>=3.12.2",
     "click>=7.1.2,!=8.0.0",  # latest version < 8.0 + blocked version with reported bug
 ]
 _nm_deps = [f"{'sparsezoo' if is_release else 'sparsezoo-nightly'}~={version_base}"]
