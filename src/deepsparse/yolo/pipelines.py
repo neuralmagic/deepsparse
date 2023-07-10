09993e703a672a424cc79f5e3d150fda644f1d5f
Damian <damian@neuralmagic.com>
2023-07-10 15:26:12 +0000

    revert changes to yolo pipeline

--- src/deepsparse/yolo/pipelines.py
+++ src/deepsparse/yolo/pipelines.py
@@ -10,20 +10,6 @@
 # software distributed under the License is distributed on an "AS IS" BASIS,
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
-# limitations under the Licensese.
-
-# Copyright (c) 2021 - present / Neuralmagic, Inc. All Rights Reserved.
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#    http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing,
-# software distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
 # limitations under the License.
 
 import json
@@ -166,7 +152,7 @@ class YOLOPipeline(Pipeline):
         """
         return YOLOOutput
 
-    def setup_onnx_file_path(self, **kwargs) -> str:
+    def setup_onnx_file_path(self) -> str:
         """
         Performs any setup to unwrap and process the given `model_path` and other
         class properties into an inference ready onnx file to be compiled by the
