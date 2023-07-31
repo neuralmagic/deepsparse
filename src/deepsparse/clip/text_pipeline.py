36a41f99f6e784a975e30d534ff951e133e2e434
Dipika Sikka <dipikasikka1@gmail.com>
2023-07-31 00:47:53 +0000

    quality fix

--- src/deepsparse/clip/text_pipeline.py
+++ src/deepsparse/clip/text_pipeline.py
@@ -12,7 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-from typing import Any, Dict, List, Tuple, Type, Union
+from typing import Any, List, Type, Union
 
 import numpy as np
 from pydantic import BaseModel, Field
