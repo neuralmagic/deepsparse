e89ff486cbad2f0a5f1659943e4a4c8289da791c
mgoin <michael@neuralmagic.com>
2023-08-07 18:06:30 -0400

    Style

--- examples/openai-server/client.py
+++ examples/openai-server/client.py
@@ -1,4 +1,19 @@
+# Copyright (c) 2021 - present / Neuralmagic, Inc. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing,
+# software distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 import openai
+
 
 # Modify OpenAI's API values to use the DeepSparse API server.
 openai.api_key = "EMPTY"
@@ -13,10 +28,8 @@ model = models["data"][0]["id"]
 # Completion API
 stream = True
 completion = openai.Completion.create(
-    model=model,
-    prompt="def fib():",
-    stream=stream,
-    max_tokenss=32)
+    model=model, prompt="def fib():", stream=stream, max_tokens=32
+)
 
 print("Completion results:")
 if stream:
