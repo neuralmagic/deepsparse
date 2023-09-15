5a158ee1d13efd60b93797038aad3a7d0ff81bdd
dbogunowicz <97082108+dbogunowicz@users.noreply.github.com>
2023-09-15 18:34:13 +0200

    [Fix] Temporary suppress transformers logging (#1244)
    * supress warnings
    
    * simplify

--- src/deepsparse/transformers/pipelines/pipeline.py
+++ src/deepsparse/transformers/pipelines/pipeline.py
@@ -17,6 +17,7 @@ Base Pipeline class for transformers inference pipeline
 """
 
 import json
+import logging
 import os
 import warnings
 from pathlib import Path
@@ -151,10 +152,14 @@ class TransformersPipeline(Pipeline, Bucketable):
             else:
                 self.config_path = os.path.join(self.config, "config.json")
 
+            hf_logger = logging.getLogger("transformers")
+            hf_logger_level = hf_logger.level
+            hf_logger.setLevel(logging.ERROR)
             self.config = transformers.PretrainedConfig.from_pretrained(
                 self.config,
                 finetuning_task=self.task if hasattr(self, "task") else None,
             )
+            hf_logger.setLevel(hf_logger_level)
 
         if isinstance(self.tokenizer, (str, Path)):
             self.tokenizer_config_path = os.path.join(self.tokenizer, "tokenizer.json")
