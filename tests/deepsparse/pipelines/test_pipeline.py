387c4a104b0b4a2eda10bcbe606a4fb6e565fcb5
Damian <damian@neuralmagic.com>
2023-09-18 10:11:43 +0000

    quality

--- tests/deepsparse/pipelines/test_pipeline.py
+++ tests/deepsparse/pipelines/test_pipeline.py
@@ -17,6 +17,7 @@ from concurrent.futures import ThreadPoolExecutor
 from unittest import mock
 
 import numpy
+
 import pytest
 from deepsparse.base_pipeline import BasePipeline
 from deepsparse.pipeline import (
