abf2a23a72c0cdcc2dc055dcc551ce6b408bf9df
Dipika Sikka <dipikasikka1@gmail.com>
2023-09-18 21:22:06 -0400

    update tests

--- tests/deepsparse/transformers/pipelines/test_text_generation.py
+++ tests/deepsparse/transformers/pipelines/test_text_generation.py
@@ -411,14 +411,12 @@ class TestTextGenerationPipeline:
         output_sequences = pipeline(
             sequences=[self.prompt], num_generated_predictions=2
         )
-
         assert len(output_sequences.generations) == 1
         assert len(output_sequences.generations[0]) == 2
 
         output_sequences = pipeline(
             sequences=[self.prompt, self.prompt], num_generated_predictions=2
         )
-
         assert len(output_sequences.generations) == 2
 
         for generation in output_sequences.generations:
