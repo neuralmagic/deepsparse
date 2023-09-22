0246f8944b53d05608d6f4d5c1a7b81da4ed85ef
mgoin <michael@neuralmagic.com>
2023-09-22 13:52:15 +0000

    Format

--- src/deepsparse/transformers/utils/helpers.py
+++ src/deepsparse/transformers/utils/helpers.py
@@ -87,7 +87,7 @@ def initialize_kv_cache_state(
             # temporarily ignoring `empty` as passing the empty
             # kv cache array is currently causing a segfault in the
             # internal kv-cache runtime
-            batch_size, # if not empty else 0,
+            batch_size,  # if not empty else 0,
             num_attention_heads,
             length if length is not None else length_,
             hidden_dims,
