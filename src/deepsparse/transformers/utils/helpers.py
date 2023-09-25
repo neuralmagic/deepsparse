0b766abbeb89eec305fa555ca1a293d4ac0bfa66
Dipika Sikka <dipikasikka1@gmail.com>
2023-09-25 13:47:58 -0400

    Quality

--- src/deepsparse/transformers/utils/helpers.py
+++ src/deepsparse/transformers/utils/helpers.py
@@ -11,9 +11,7 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-import json
 import logging
-import os
 import pathlib
 import uuid
 from typing import Any, Dict, List, Optional, Tuple, Union
