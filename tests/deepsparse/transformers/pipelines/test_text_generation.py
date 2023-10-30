e6e78286ccbbe17e2629fcd6070831ca749e0cde
Alexandre Marques <alexandre@neuralmagic.com>
2023-10-30 17:42:39 -0400

    Rebase

--- tests/deepsparse/transformers/pipelines/test_text_generation.py
+++ tests/deepsparse/transformers/pipelines/test_text_generation.py
@@ -19,3 +19,98 @@ import numpy
 import pytest
 from deepsparse import Pipeline
 from deepsparse.transformers.utils.helpers import prepends_bos_token
+
+
+@pytest.fixture
+def pipeline():
+    return Pipeline.create(
+        task="text_generation",
+        model_path="hf:mgoin/TinyStories-1M-deepsparse",
+        engine_type="onnxruntime",
+    )
+
+
+@pytest.fixture
+def prompt():
+    return "Never gonna give you up, never gonna let you down"
+
+
+def test_freeze_first_position(pipeline):
+    # Test whether we should be "freezing" the first token after
+    # the kv cache is full
+    assert not prepends_bos_token(pipeline.tokenizer)
+
+
+def test_run_same_prompt_multiple_times(pipeline, prompt):
+    # Test the scenario, where the same prompt is run multiple times
+    # Every run should produce the same output
+    output_1 = pipeline(prompt, output_scores=True)
+    output_2 = pipeline(prompt, output_scores=True)
+
+    assert output_1.generations[0].text == output_2.generations[0].text
+    assert numpy.allclose(
+        output_1.generations[0].score,
+        output_2.generations[0].score,
+        atol=1e-3,
+    )
+
+
+def test_run_multiple_prompts_in_parallel(pipeline, prompt):
+    # Test the scenario, where multiple prompts are run in parallel
+    # Same two prompts should produce the same output
+
+    output = pipeline([prompt, prompt], output_scores=True)
+
+    logits_0 = output.generations[0].score
+    sequence_0 = output.generations[0].text
+
+    logits_1 = output.generations[1].score
+    sequence_1 = output.generations[1].text
+
+    assert numpy.allclose(logits_0, logits_1, atol=1e-3)
+    assert sequence_0 == sequence_1
+
+
+def test_num_generated_predictions(pipeline, prompt):
+    # Test the scenario, where multiple predictions are generated
+    # from the same prompt
+
+    output_sequences = pipeline(prompt, num_return_sequences=2)
+
+    assert len(output_sequences.generations) == 1
+    assert len(output_sequences.generations[0]) == 2
+
+    output_sequences = pipeline([prompt, prompt], num_return_sequences=2)
+    assert len(output_sequences.generations) == 2
+
+    for generation in output_sequences.generations:
+        assert len(generation) == 2
+
+
+def test_token_generation_deterministic(pipeline, prompt):
+    inference = pipeline(prompt, num_return_sequences=3, do_sample=False)
+    generations = inference.generations
+    # Output should be the same from one another
+    text_outputs = [x.text for x in generations[0]]
+    assert len(set(text_outputs)) == 1
+
+
+def test_token_generation_non_deterministic(pipeline, prompt):
+
+    inference = pipeline(prompt, num_return_sequences=3, do_sample=True)
+    generations = inference.generations
+    # Output should be different from one another
+    text_outputs = [x.text for x in generations[0]]
+    assert len(set(text_outputs)) == 3
+
+
+def test_streaming_mode_returns_generator(pipeline, prompt):
+    response_generator = pipeline(prompt, streaming=True)
+    assert inspect.isgenerator(
+        response_generator
+    ), "Pipeline should return a generator in streaming mode"
+
+    assert all(
+        isinstance(response, pipeline.output_schema) for response in response_generator
+    ), "Pipeline should return a generator of output_schema \
+           objects in streaming mode"
