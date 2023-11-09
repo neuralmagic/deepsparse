320b9c27a3b57226ab5a4b5f3b60050f23a508a6
dbogunowicz <97082108+dbogunowicz@users.noreply.github.com>
2023-11-09 11:48:39 +0100

    [Evaluator] Implementation of `Evaluation` (output schema for `evaluation` module) (#1390)
    * initial commit
    
    * implement serialization
    
    * massaging the PR
    
    * ready for review
    
    * print results after the evaluation is finished

--- tests/deepsparse/utils/test_data.py
+++ tests/deepsparse/utils/test_data.py
@@ -13,8 +13,16 @@
 # limitations under the License.
 
 import numpy
+import numpy as np
+from pydantic import BaseModel
 
-from deepsparse.utils import join_engine_outputs, split_engine_inputs
+import pytest
+from deepsparse.utils import (
+    join_engine_outputs,
+    prep_for_serialization,
+    split_engine_inputs,
+)
+from tests.server.test_helpers import DummyOutputSchema
 
 
 def test_split_engine_inputs():
@@ -54,3 +62,41 @@ def test_split_engine_inputs_uneven_pads():
 
     for i, j in zip(inp, joined):
         assert (i == j).all()
+
+
+@pytest.mark.parametrize(
+    "unserialized_output, target_serialized_output",
+    [
+        (
+            DummyOutputSchema(
+                field_1=[np.array([[1, 2, 3]])],
+                field_2={"key_1": np.array([[[1, 2, 3]]])},
+                field_3=DummyOutputSchema(field_1=np.array([0])),
+            ),
+            DummyOutputSchema(
+                field_1=[[[1, 2, 3]]],
+                field_2={"key_1": [[[1, 2, 3]]]},
+                field_3=DummyOutputSchema(field_1=[0]),
+            ),
+        )
+    ],
+)
+def test_prep_for_serialization(unserialized_output, target_serialized_output):
+    def check_dict_equality(dict_1, dict_2):
+        for key, value in dict_1.items():
+            if isinstance(value, BaseModel):
+                value = value.dict()
+                check_dict_equality(value, dict_2[key].dict())
+            elif isinstance(value, dict):
+                check_dict_equality(value, dict_2[key])
+            elif isinstance(value, list):
+                equal = value == dict_2[key]
+                equal = equal if isinstance(equal, bool) else equal.all()
+                assert equal
+            else:
+                assert value == dict_2[key]
+
+    serialized_output = prep_for_serialization(unserialized_output)
+    serialized_output = serialized_output.dict()
+    target_serialized_output = target_serialized_output.dict()
+    check_dict_equality(target_serialized_output, serialized_output)
