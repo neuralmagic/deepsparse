bc9df5b1e3cd8fee1f107f98a5d2fab3692e79d9
Michael Goin <michael@neuralmagic.com>
2023-11-10 11:24:32 -0500

    add print

--- tests/deepsparse/loggers/metric_functions/predefined/test_predefined.py
+++ tests/deepsparse/loggers/metric_functions/predefined/test_predefined.py
@@ -159,8 +159,17 @@ def test_group_name(mock_engine, group_name, pipeline_name, inputs, optional_ind
 
     with open(expected_logs, "r") as f:
         expected_logs = f.read().splitlines()
-    for log, expected_log in zip(data_logging_logs, expected_logs):
-        assert log == expected_log
+
+    data_logging_logs = set(data_logging_logs)  # Convert to a set for efficient search
+
+    print("data_logging_logs:")
+    for l in data_logging_logs:
+        print(l)
+
+    for expected_log in expected_logs:
+        assert (
+            expected_log in data_logging_logs
+        ), f"Missing expected log: {expected_log}"
 
 
 yaml_config = """
