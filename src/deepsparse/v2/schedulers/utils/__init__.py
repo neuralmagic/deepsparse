20980a769f2abe89f0247c5f7e58193286af9217
Benjamin Fineran <bfineran@users.noreply.github.com>
2023-11-13 16:12:35 -0500

    [Continuous Batching] Queue Implementation to support batching grouping and prioritization (#1373)
    * [Continuous Batching] Queue Implementation to support batching grouping and prioritization
    
    * has_key method
    
    * thread safety
    
    * add blocking option for pop_batch
    
    * update docstring
    
    * allow mutex to be shared across continuous batching objects
    
    * revert last commit

--- src/deepsparse/v2/schedulers/utils/__init__.py
+++ src/deepsparse/v2/schedulers/utils/__init__.py
@@ -14,5 +14,5 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-from .operator import *
-from .engine_operator import *
+
+from .continuous_batching_queues import *
