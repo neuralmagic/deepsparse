7d6571e6899e8e20e36ddebca79c2fe0f97aa961
george <george@quad-mle>
2023-11-29 16:37:03 +0000

    middleware manager, add dispatch

--- src/deepsparse/v2/middleware/middleware_manager.py
+++ src/deepsparse/v2/middleware/middleware_manager.py
@@ -18,15 +18,37 @@ from typing import List
 
 class MiddlewareManager:
     def __init__(self, middlewares: List):
-        self.middlwares = {}
+        self.middlewares = {}
         self._init(middlewares)
 
     def _init(self, middleware: List):
-        for middleware in self.middlewares:
-            self.middlwares[middleware.__name__] = middleware
+        for middleware in middleware:
+            init_middleware = middleware()
+            self.middlewares[init_middleware.__name__] = init_middleware
 
     def start_event(self, name: str, *args, **kwargs):
-        self.middlwares[name].start_event(*args, **kwargs)
+        self.middlewares[name].start_event(*args, **kwargs)
 
     def end_event(self, name: str, *args, **kwargs):
-        self.middlwares[name].start_event(*args, **kwargs)
+        self.middlewares[name].end_event(*args, **kwargs)
+
+    def dispatch(self, command: str, *args, **kwargs):
+        """
+        command should include both the middleware
+         key and the function inside the selected middleware
+
+        dispatch("timer.update_middleware_timer", *args, **kwargs)
+        """
+        name, func_name = command.split(".")
+        if name in self.middlewares:
+            target_func = self.middlewares[name]
+            method = getattr(target_func, func_name, None)
+            if callable(method):
+                return method(*args, **kwargs)
+
+    def __getitem__(self, key):
+        return self.middlewares[key]
+
+    def iterate_dict(self):
+        for key in self.middlewares:
+            yield key, self.middlewares[key]
