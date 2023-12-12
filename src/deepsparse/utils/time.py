9caaae7c01e5875747e8be8aa4367ef16f14edbc
George Ohashi <george@neuralmagic.com>
2023-12-12 16:19:46 +0000

    revert timer.py

---
+++ src/deepsparse/utils/time.py
@@ -0,0 +1,46 @@
+
+import time
+from contextlib import contextmanager
+import threading
+from typing import Dict
+
+class Timer:
+    def __init__(self):
+        self._lock = threading.Lock()
+        self.start_time = {}
+        self.measurements = {}
+
+    def start(
+        self,
+        id: str,
+    ):
+        with self._lock:
+            self.start_time[id] = time.time()
+
+    def stop(
+        self,
+        id: str,
+    ):
+        end_time = time.time()
+        with self._lock:
+            if id in self.start_time:
+                start_time = self.start_time[id]
+                del self.start_time[id]
+                self.measurements[id] = end_time - start_time
+
+    @contextmanager
+    def record(self, id: str):
+        start = time.time()
+        yield
+        with self._lock:
+            self.measurements[id] = time.time() - start
+
+class TimerManager:
+    def __init__(self):
+        self.measurements = {}
+        
+    def get_new_timer(self):
+        return Timer()
+    
+    def update(self, measurements: Dict[str, float]):
+        self.measurements.update(measurements)
