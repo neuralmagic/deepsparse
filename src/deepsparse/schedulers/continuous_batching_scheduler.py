dbbfa3c1a9290b3a1d8ca5c9f3cb71f7476586ad
Dipika Sikka <dipikasikka1@gmail.com>
2024-01-05 15:50:53 +0000

    quick comment for asyncio future vs concurrent

--- src/deepsparse/schedulers/continuous_batching_scheduler.py
+++ src/deepsparse/schedulers/continuous_batching_scheduler.py
@@ -113,6 +113,9 @@ class ContinuousBatchingScheduler(OperatorScheduler):
                 f"found {type(inputs)}"
             )
 
+        # asyncio.Future() is used when we run the pipeline using async/excecute
+        # operators using asyncio. Outside of the async pathway
+        # (i.e. outside of the server), we use concurrent.Future()
         future = asyncio.Future() if kwargs.get("loop") else Future()
         self._queues.add_queue_item(key=operator, item=inputs, future=future)
 
