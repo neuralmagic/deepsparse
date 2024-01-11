2e31d9083590ccedf6dd28cec9dd0de667587b8c
Dipika Sikka <dipikasikka1@gmail.com>
2024-01-11 22:58:48 +0000

    import

--- tests/deepsparse/schedulers/utils/test_continuous_batching_executor.py
+++ tests/deepsparse/schedulers/utils/test_continuous_batching_executor.py
@@ -11,12 +11,12 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-
 import time
 from concurrent.futures import Future
 
 import numpy
 
+import pytest
 from deepsparse.operators import EngineOperator
 from deepsparse.schedulers.utils import (
     ContinuousBatchingExecutorThread,
