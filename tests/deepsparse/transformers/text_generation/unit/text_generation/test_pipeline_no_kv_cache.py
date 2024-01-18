2b6b7c6ff75ef6320983f4e33a0162d561ce1a97
dbogunowicz <damian@neuralmagic.com>
2024-01-18 10:23:10 +0000

    add a useful test

--- tests/deepsparse/transformers/text_generation/unit/text_generation/test_pipeline_no_kv_cache.py
+++ tests/deepsparse/transformers/text_generation/unit/text_generation/test_pipeline_no_kv_cache.py
@@ -12,6 +12,8 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+import copy
+
 from deepsparse import TextGeneration
 from deepsparse.transformers.pipelines.text_generation.pipeline import (
     TextGenerationPipeline,
@@ -33,3 +35,25 @@ def test_create_pipeline_with_no_kv_cache_support(model_attributes):
         model_path=model_attributes[1], onnx_model_name="model-orig.onnx"
     )
     assert isinstance(pipeline, TextGenerationPipelineNoCache)
+
+
+def test_assert_same_outputs_regardless_of_kv_cache_support(model_attributes):
+    # make sure that kv cache support does not change the output
+    prompt = "Hello, how are you doing today?"
+    _prompt = copy.deepcopy(prompt)
+    max_new_tokens = 16
+    pipeline = TextGeneration(
+        model_path=model_attributes[1], onnx_model_name="model-orig.onnx"
+    )
+    for _ in range(max_new_tokens):
+        # simulate autoregressive generation with non-kv cache pipeline
+        out = pipeline(prompt=_prompt)
+        new_token = pipeline.tokenizer.encode(out.generations[0].text)
+        old_tokens = pipeline.tokenizer.encode(_prompt)
+        _prompt = pipeline.tokenizer.decode(old_tokens + new_token)
+    # max_new_tokens reduced by one, because the pipeline always grabs
+    # the first generated token from the prefill
+    out = TextGeneration(model_path=model_attributes[1])(
+        prompt=prompt, max_new_tokens=max_new_tokens - 1
+    )
+    assert _prompt == prompt + out.generations[0].text
