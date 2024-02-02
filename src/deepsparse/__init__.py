a4151ed1f1fb08d6e7b026b1472bc0798ba22d1f
rahul-tuli <rahul@neuralmagic.com>
2024-02-02 05:21:06 -0500

    Add evaluate callable

--- src/deepsparse/__init__.py
+++ src/deepsparse/__init__.py
@@ -38,5 +38,6 @@ from .loggers import *
 from .version import __version__, is_release
 from .analytics import deepsparse_analytics as _analytics
 from .subgraph_execute import *
+from .evaluation.evaluator import evaluate
 
 _analytics.send_event("python__init")
