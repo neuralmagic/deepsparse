ccd613f9660624daa64214760c7dc68ce1ff5158
Jeannie Finks <74554921+jeanniefinks@users.noreply.github.com>
2025-05-29 20:00:06 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,4 +1,4 @@
-# Copyright (c) 2021 - present / Neuralmagic, Inc. All Rights Reserved.
+# Copyright (c) 2021 - 2025 / Neuralmagic, Inc. All Rights Reserved.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
